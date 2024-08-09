66038a15b79583013766db9be262bf48093d8bba
Jhyden Ellana <jhydenellana@gmail.com>
2024-08-09 17:17:05 +0800

    📦️ Add vue component

--- packages/file-icons/vite.config.ts
+++ packages/file-icons/vite.config.ts
@@ -1,11 +1,18 @@
 import { resolve } from "node:path";
-import { defineConfig, mergeConfig } from "vite";
+import { defineConfig, mergeConfig, searchForWorkspaceRoot } from "vite";
+import vue from "@vitejs/plugin-vue";
 import conf from "../../vite.config";
 
 // https://vitejs.dev/config/
 export default mergeConfig(
   conf,
   defineConfig({
+    plugins: [vue()],
+    server: {
+      fs: {
+        allow: [searchForWorkspaceRoot(process.cwd())],
+      },
+    },
     build: {
       lib: {
         entry: resolve(__dirname, "./src/index.ts"),
