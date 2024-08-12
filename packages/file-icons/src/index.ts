4e9996096274a55d372a8f57f72f51f0a49b2986
Jhyden Ellana <jhydenellana@gmail.com>
2024-08-12 09:25:24 +0800

    ♻️ Export vue component

--- packages/file-icons/src/index.ts
+++ packages/file-icons/src/index.ts
@@ -1,5 +1,13 @@
 import "./css/index.scss";
 import FileIcon from "./vue/file-icons.vue";
 import { catalog, setPaths, appendChildPath, clearChildPaths } from "./fi";
+import VueComponent from "./vue/file-icons.vue";
 
-export { catalog, FileIcon, setPaths, appendChildPath, clearChildPaths };
+export {
+  catalog,
+  FileIcon,
+  VueComponent,
+  setPaths,
+  appendChildPath,
+  clearChildPaths,
+};
