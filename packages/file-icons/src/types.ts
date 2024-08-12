768891a253e34eee3b77018a7ac2ac623ee7dd48
Jhyden Ellana <jhydenellana@gmail.com>
2024-08-12 09:23:50 +0800

    💡 Update 'types' declaration comments - Add defaults

--- packages/file-icons/src/types.ts
+++ packages/file-icons/src/types.ts
@@ -1,8 +1,16 @@
 export type ICatalog = Record<string, number>;
 
 export type IOptions = {
-  // Element tag to use for the paths
+  /**
+   * Element tag to use for the paths
+   * @default span
+   */
   tag?: string;
-  // Class prefix to use for the paths' classname
+
+  /**
+   * Class prefix to use for the path's classname
+   * (use only if you have custom css styling)
+   * @default path
+   */
   classPrefix?: string;
 };
