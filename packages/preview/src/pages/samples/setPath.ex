0420babb07b514c1304986d9f6663d6dd8f598e8
Jhyden Ellana <jhydenellana@gmail.com>
2024-08-13 11:06:40 +0800

    📝 Readme update - Fixed typo

--- packages/preview/src/pages/samples/setPath.ex
+++ packages/preview/src/pages/samples/setPath.ex
@@ -12,10 +12,10 @@
       <span class="fi fi-cmd" style="font-size: 75pt"></span>
     </div>
     <script type="module">
-      import "@whykhamist/vue-file-icons/style.css";
+      import "@whykhamist/file-icons/style.css";
 
       //Just call the setPaths function after the DOM is ready
-      import { setPaths } from "@whykhamist/vue-file-icons";
+      import { setPaths } from "@whykhamist/file-icons";
 
       window.addEventListener("DOMContentLoaded", setPaths());
     </script>
